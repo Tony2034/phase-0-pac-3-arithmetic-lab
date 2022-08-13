48d9dd2932bbfa3188310938a9ab9a4fc11b3f48
Tonny Blair Okumu <hellotony2034@gmail.com>
2022-08-13 16:59:16 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+let a = 10
+let b = 10
+function add (a,b) {
+   return (a+b)
+}
+function subtract (a,b) {
+    return (a-b)
+}
+function multiply (a,b){
+    return (a*b)
+}
+function divide (a,b) {
+    return (a/b)
+}
+let n = 200
+function increment (n) {
+    return ++n
+}
+function decrement (n) {
+    return --n
+}
+function makeInt (n) {
+    return parseInt(n,10)
+}
+function preserveDecimal (n) {
+    return parseFloat(n)
+}
